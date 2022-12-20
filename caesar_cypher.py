3cc49fb0581fd41c289f4d999340144bf41ef04a
Mittie Delsie <mittiede@ev-o5.kzn.21-school.ru>
2022-12-20 17:37:00 +0300

    caesar_brute_force

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,28 +1,30 @@
 class Caesar:
-    def __init__(self, key, message):
+    def __init__(self, message):
         self.symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
         self.message = message
         self.answer = ''
         self.encrypted = ''
-        self.key = key
 
-    def encrypt(self):
+    def encrypt(self, key):
         for i in range(len(self.message)):
             if message[i] == ' ':
                 self.answer += ' '
                 continue
-            pos = self.key + self.symbols.index(self.message[i])
+            pos = key + self.symbols.index(self.message[i])
             if pos >= len(self.symbols):
                 while pos >= len(self.symbols):
                     pos -= len(self.symbols)
             self.answer += self.symbols[pos]
 
-    def decrypt(self):
+    def decrypt(self, key):
         for i in range(len(self.answer)):
             if self.answer[i] == ' ':
                 self.encrypted += ' '
                 continue
-            pos = self.symbols.index(self.answer[i]) - self.key
+            pos = self.symbols.index(self.answer[i]) - key
+            if pos <= -len(self.symbols):
+                while pos <= -len(self.symbols):
+                    pos += len(self.symbols)
             self.encrypted += self.symbols[pos]
 
     def choose(self, choose):
@@ -33,11 +35,16 @@ class Caesar:
         else:
             return -1
 
+    def brute_decrypt(self):
+        for i in range(1, len(self.message)):
+            self.encrypted = ''
+            self.decrypt(i)
+            print(self.encrypted)
+
 message = input('enter your message: ')
 key = int(input('enter key: '))
-caesar = Caesar(key, message)
-caesar.encrypt()
+caesar = Caesar(message)
+caesar.encrypt(key)
 print(caesar.answer)
-caesar.decrypt()
-print(caesar.encrypted)
+caesar.brute_decrypt()
 
