1aed5c0c992f8de8bc5087d78706d512b4aa6955
Mittie Delsie <mittiede@ev-o5.kzn.21-school.ru>
2022-12-20 17:04:12 +0300

    caesar_code_problem_with_list

---
+++ caesar_cypher.py
@@ -0,0 +1,46 @@
+class Caesar:
+    def __init__(self, key, message):
+        self.symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        self.message = message
+        self.answer = ''
+        self.encrypted = ''
+        self.key = key
+
+    def encrypt(self):
+        for i in range(len(self.message)):
+            if message[i] == ' ':
+                self.answer += ' '
+                continue
+            pos = self.key + self.symbols.index(self.message[i])
+            if pos > len(self.symbols):
+                while pos > len(self.symbols):
+                    pos -= len(self.symbols)
+            self.answer += self.symbols[pos]
+
+    def decrypt(self):
+        for i in range(len(self.answer)):
+            if self.answer[i] == ' ':
+                self.encrypted += ' '
+                continue
+            pos = self.symbols.index(self.answer[i]) - self.key
+            if pos < 0:
+                pos *= -1
+            self.encrypted += self.symbols[pos]
+
+
+    def choose(self, choose):
+        if choose in ('encrypt', 'e'):
+            self.encrypt()
+        elif choose in ('decrypt', 'd'):
+            self.decrypt()
+        else:
+            return -1
+
+message = input('enter your message: ')
+key = int(input('enter key: '))
+caesar = Caesar(key, message)
+caesar.encrypt()
+print(caesar.answer)
+caesar.decrypt()
+print(caesar.encrypted)
+
