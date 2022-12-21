1579279473fa97c8f27db5bad4eecdd0feefb8e4
Mittie Delsie <mittiede@ev-o5.kzn.21-school.ru>
2022-12-21 20:56:35 +0300

    not_correct_scoring

--- reversegam.py
+++ reversegam.py
@@ -30,7 +30,7 @@ class Reversegam:
 
     def scoring(self, pos, side, opposite_side):
         line, column = pos
-        if self.board[line][column] != ' ':
+        if not self.is_valid_move(pos):
             return False
         for i in self.directions:
             new_pos = (i[0] + line, i[1] + column)
@@ -48,9 +48,62 @@ class Reversegam:
                     continue
         self.place_on_map(pos, side)
 
-game = Reversegam(1, 2)
+    def most_score(self, pos):
+        score = 0
+        line, column = pos
+        for i in self.directions:
+            try:
+                new_pos = (i[0] + line, i[1] + column)
+                if self.board[new_pos[0]][new_pos[1]] == self.player_side:
+                 try:
+                     if self.board[new_pos[0] + i[0]][new_pos[1] + i[1]] == self.computer_side:
+                         score += 1
+                 except:
+                     continue
+            except:
+                continue
+        return score
+
+    def check_every_move(self):
+        maxi = 0
+        res_pos = ()
+        for line in range(len(self.board)):
+            for column in range(len(self.board)):
+                if self.is_valid_move((line, column)):
+                    res = self.most_score((line, column))
+                    if res > maxi:
+                        maxi = res
+                        res_pos = (line, column)
+        self.scoring(res_pos, self.computer_side, self.player_side)
+
+    def is_valid_move(self, pos):
+        line, column = pos
+        return self.board[line][column] == ' '
+    
+    def take_corner(self):
+        pass
+
+    def decide(self):
+        pass
+    
+    def is_end(self):
+        for line in self.board:
+            if ' ' in line:
+                return False
+        return True
+
+    def who_won(self):
+        return 'computer' if self.computer_score > self.player_score else 'player'
+
+game = Reversegam('X', 'O')
 game.fill_board()
-game.draw_map()
-game.scoring((4, 2), 'X', 'O')
-game.draw_map()
+while True:
+    game.draw_map()
+    pos = list(map(int, input("make your move: ").split()))
+    game.scoring(pos, 'X', 'O')
+    game.draw_map()
+    game.check_every_move()
+    if game.is_end():
+        game.draw_map()
+        break
 
