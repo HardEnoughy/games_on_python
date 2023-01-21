7bfb618107c542a63b1596345ea916b8a40564fa
mittiede <sudakovkonst@yandex.ru>
2023-01-21 13:27:03 +0300

    game_over_window_added

--- Pygame/dodger_game.py
+++ Pygame/dodger_game.py
@@ -1,12 +1,16 @@
 import pygame, sys, random
 from pygame.locals import *
 
+pygame.init()
+
 #constants
 PLAYERSPEED = 6
 WHITE = (255, 255, 255)
-NEWENEMY = 7
-WINDOWWIDTH = 400
-WINDOWHEIGHT = 400
+NEWENEMY = 12
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 800
+PLAYERSIZE = 50
+BLUE = (0, 0, 255)
 
 class DodgerGame():
     def __init__(self):
@@ -17,14 +21,13 @@ class DodgerGame():
         pygame.display.set_caption("Dodger game")
 
         #player setting
-        self.player = pygame.Rect(200, 380, 40, 40)
+        self.player = pygame.Rect(WINDOWWIDTH // 2, WINDOWHEIGHT - PLAYERSIZE, PLAYERSIZE, PLAYERSIZE)
         self.player_image = pygame.image.load("images/player.png")
 
         #baddies settings
         self.baddie_image = pygame.image.load("images/baddie.png")
         self.baddies_stretch = []
         self.baddies = []
-        self.baddies_counter = 0
         for i in range(10):
             self.add_baddie()
         
@@ -33,6 +36,50 @@ class DodgerGame():
         self.is_right = False
         self.is_up = False
         self.is_down = False
+
+        #text for end game
+        self.main_font = pygame.font.SysFont(None, 48)
+        self.game_over_text = self.main_font.render("GAME OVER", False, WHITE, BLUE)
+        self.font = pygame.font.SysFont(None, 30)
+        self.after_game_text = self.font.render("Do you want to try again? Press space for again and esc for exit", False, WHITE, BLUE)
+    
+    def show_game_over(self):
+        #game over text
+        game_over_rect = self.game_over_text.get_rect()
+        game_over_rect.centerx = self.main_surface.get_rect().centerx
+        game_over_rect.centery = self.main_surface.get_rect().centery
+
+        #hint text
+        after_game_rect = self.after_game_text.get_rect()
+        after_game_rect.centerx = self.main_surface.get_rect().centerx
+        after_game_rect.centery = self.main_surface.get_rect().centery + 50
+
+        self.main_surface.fill(BLUE)
+
+        self.main_surface.blit(self.game_over_text, game_over_rect)
+        self.main_surface.blit(self.after_game_text, after_game_rect)
+
+        pygame.display.update()
+
+        is_again = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == K_SPACE:
+                        is_again = True 
+                    
+            if is_again:
+                break
+        
+        self.run_game()
+
     
     def add_baddie(self):
         size = random.randint(5, 40)
@@ -47,12 +94,23 @@ class DodgerGame():
         self.main_surface.blit(self.baddies_stretch[self.baddies.index(baddie)], baddie)
     
     def run_game(self):
+        self.main_surface.fill(WHITE)
+        self.baddies = []
+        self.baddies_stretch = []
+        self.is_left = False
+        self.is_right = False
+        self.is_up = False
+        self.is_down = False
+        pygame.display.update()
+        is_game_over = False
         while True:
+            #event checks
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
                 
+                #keydown events 
                 if event.type == KEYDOWN:
                     if event.key == K_LEFT:
                         self.is_left = True
@@ -62,7 +120,8 @@ class DodgerGame():
                         self.is_down = True
                     if event.key == K_UP:
                         self.is_up = True
-
+                
+                #keyup events
                 if event.type == KEYUP:
                     if event.key == K_LEFT:
                         self.is_left = False 
@@ -73,11 +132,14 @@ class DodgerGame():
                     if event.key == K_UP:
                         self.is_up = False
             
+            #adding baddies if there is not enough
             while len(self.baddies) < NEWENEMY:
                 self.add_baddie()
             
+            #filling display
             self.main_surface.fill(WHITE)
 
+            #moving player if needed
             if self.is_up and self.player.top > 0:
                 self.player.top -= PLAYERSPEED
             if self.is_right and self.player.right < WINDOWWIDTH:
@@ -87,6 +149,7 @@ class DodgerGame():
             if self.is_left and self.player.left > 0:
                 self.player.left -= PLAYERSPEED
             
+            #removing or moving baddies
             for baddie in self.baddies[:]:
                 if baddie.bottom >= WINDOWHEIGHT:
                     self.baddies.remove(baddie) 
@@ -95,14 +158,21 @@ class DodgerGame():
                     self.move_baddie(baddie)
                     self.draw_baddie(baddie)
                 
+                #checking collision
                 if self.player.colliderect(baddie):
-                    pygame.quit()
-                    sys.exit()
+                   is_game_over = True 
+            
+            if is_game_over:
+                break
                     
+            #drawing player
             self.main_surface.blit(self.player_image, self.player)
 
+            #updating display
             pygame.display.update()
-            self.main_clock.tick(40)
+            self.main_clock.tick(60)
+        
+        self.show_game_over()
 
 game = DodgerGame()
 game.run_game()
